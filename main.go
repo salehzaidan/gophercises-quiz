9d54e4df35db47eb9bc7c0dd688fbbbb1fd1c6b6
salehzaidan <salehzaidan64@gmail.com>
2024-03-07 11:42:59 +0700

    Implement time limit

--- main.go
+++ main.go
@@ -6,9 +6,13 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"time"
 )
 
-const defaultProblemsFilename = "problems.csv"
+const (
+	defaultProblemsFilename = "problems.csv"
+	defaultTimeLimit        = 30
+)
 
 type Problem struct {
 	Question string
@@ -39,19 +43,37 @@ func readProblems(filename string) ([]Problem, error) {
 	return problems, nil
 }
 
+// answerProblem prints the question of problem p at index i and reads the user answer
+// from standard input. It sends the result of the user answer as a boolean to
+// channel c.
+func answerProblem(p Problem, i int, c chan<- bool) {
+	fmt.Printf("Problem #%d: %s = ", i+1, p.Question)
+
+	var answer string
+	fmt.Scanln(&answer)
+
+	c <- answer == p.Answer
+}
+
 // answerProblems prints each question of the problem and reads the user answer
-// from standard input. It returns the number of problems answered correctly.
-func answerProblems(problems []Problem) int {
+// from standard input. It times out after limit seconds. It returns
+// the number of problems answered correctly.
+func answerProblems(problems []Problem, limit time.Duration) int {
 	correct := 0
+	answer := make(chan bool)
+	timeout := time.After(limit)
 
 	for i, p := range problems {
-		fmt.Printf("Problem #%d: %s = ", i+1, p.Question)
+		go answerProblem(p, i, answer)
 
-		var answer string
-		fmt.Scanln(&answer)
-
-		if answer == p.Answer {
-			correct++
+		select {
+		case <-timeout:
+			fmt.Println()
+			return correct
+		case isCorrect := <-answer:
+			if isCorrect {
+				correct++
+			}
 		}
 	}
 
@@ -60,6 +82,7 @@ func answerProblems(problems []Problem) int {
 
 func main() {
 	filename := flag.String("csv", defaultProblemsFilename, "a csv file in the format of 'question,answer'")
+	timeLimit := flag.Int("limit", defaultTimeLimit, "the time limit for the quiz in seconds")
 	flag.Parse()
 
 	problems, err := readProblems(*filename)
@@ -67,6 +90,6 @@ func main() {
 		log.Fatalf("read problems: %v", err)
 	}
 
-	correct := answerProblems(problems)
+	correct := answerProblems(problems, time.Duration(*timeLimit)*time.Second)
 	fmt.Printf("You scored %d out of %d\n", correct, len(problems))
 }
